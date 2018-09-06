04aabfbee3764c4b4c8c0f1840c54d1f2f974448
sergiosuperstar <keshelj@yahoo.com>
2018-09-06 12:41:45 +0200

    update nugets; update axe-core js; update tests;

--- Globant.Selenium.Axe/Globant.Selenium.Axe/AxeBuilder.cs
+++ Globant.Selenium.Axe/Globant.Selenium.Axe/AxeBuilder.cs
@@ -1,7 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using System;
-using System.Net;
 
 namespace Globant.Selenium.Axe
 {
@@ -14,15 +14,15 @@ namespace Globant.Selenium.Axe
         private readonly IWebDriver _webDriver;
         private readonly IncludeExcludeManager _includeExcludeManager = new IncludeExcludeManager();
 
-        private static readonly AxeBuilderOptions DefaultOptions = new AxeBuilderOptions {ScriptProvider = new EmbeddedResourceAxeProvider()};
+        private static readonly AxeBuilderOptions DefaultOptions = new AxeBuilderOptions { ScriptProvider = new EmbeddedResourceAxeProvider() };
 
-    	 public string Options { get; set; } = "null";
-
+        public string Options { get; set; } = "null";
+        public bool WaitForResult { get; set; } = true;
         /// <summary>
         /// Initialize an instance of <see cref="AxeBuilder"/>
         /// </summary>
         /// <param name="webDriver">Selenium driver to use</param>
-        public AxeBuilder(IWebDriver webDriver): this(webDriver, DefaultOptions)
+        public AxeBuilder(IWebDriver webDriver) : this(webDriver, DefaultOptions)
         {
         }
 
@@ -33,13 +33,12 @@ namespace Globant.Selenium.Axe
         /// <param name="options">Builder options</param>
         public AxeBuilder(IWebDriver webDriver, AxeBuilderOptions options)
         {
-            if (webDriver == null)
-                throw new ArgumentNullException(nameof(webDriver));
-
             if (options == null)
+            {
                 throw new ArgumentNullException(nameof(options));
+            }
 
-            _webDriver = webDriver;
+            _webDriver = webDriver ?? throw new ArgumentNullException(nameof(webDriver));
             _webDriver.Inject(options.ScriptProvider);
         }
 
@@ -50,9 +49,17 @@ namespace Globant.Selenium.Axe
         /// <param name="args"></param>
         private AxeResult Execute(string command, params object[] args)
         {
-            object response = ((IJavaScriptExecutor)_webDriver).ExecuteAsyncScript(command, args);
+            ((IJavaScriptExecutor)_webDriver).ExecuteScript(command, args);
+
+            if (WaitForResult)
+            {
+                var wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(60));
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.Id("accessibilityResultReady")));
+            }
+
+            object response = ((IJavaScriptExecutor)_webDriver).ExecuteScript("return window.promiseResultAccessibility;", args);
             var jObject = JObject.FromObject(response);
-            return new AxeResult(jObject);   
+            return new AxeResult(jObject);
         }
 
         /// <summary>
@@ -62,7 +69,7 @@ namespace Globant.Selenium.Axe
         /// <returns></returns>
         public AxeBuilder Include(params string[] selectors)
         {
-            _includeExcludeManager.Include(selectors);
+            _includeExcludeManager.IncludeToAnalyzer(selectors);
             return this;
         }
 
@@ -74,7 +81,7 @@ namespace Globant.Selenium.Axe
         /// <returns></returns>
         public AxeBuilder Exclude(params string[] selectors)
         {
-            _includeExcludeManager.Exclude(selectors);
+            _includeExcludeManager.ExcludeToAnalyzer(selectors);
             return this;
         }
 
@@ -85,8 +92,20 @@ namespace Globant.Selenium.Axe
         /// <returns>An aXe results document</returns>
         public AxeResult Analyze(IWebElement context)
         {
-            string command = string.Format("axe.a11yCheck(arguments[0], {0}, arguments[arguments.length - 1]);", Options);
-            return Execute(command, context);
+            var id = context.GetAttribute("id");
+            if (string.IsNullOrEmpty(id)){
+                throw new ArgumentException("WebElement must have a valid id");
+            }
+
+            var commandParams = Options == "null" ? $"document.getElementById('{id}')" : $"document.getElementById('{id}'), {Options}";
+            var fullCommand = @"window.promiseResultAccessibility = null;
+                                axe.run([params]).then(function (result) {
+                                promiseResultAccessibility = result;
+                                $(document).add('div').attr('id','accessibilityResultReady');
+                            });
+                        ";
+            fullCommand = fullCommand.Replace("[params]", commandParams);
+            return Execute(fullCommand, context);
         }
 
         /// <summary>
@@ -95,23 +114,29 @@ namespace Globant.Selenium.Axe
         /// <returns>An aXe results document</returns>
         public AxeResult Analyze()
         {
-            string command;
+            string commandParams;
 
             if (_includeExcludeManager.HasMoreThanOneSelectorsToIncludeOrSomeToExclude())
             {
-                command = $"axe.a11yCheck({_includeExcludeManager.ToJson()}, {Options}, arguments[arguments.length - 1]);";
+                commandParams = Options == "null" ? $"{ _includeExcludeManager.ToJson()}" : $"{ _includeExcludeManager.ToJson()}, {Options}";
             }
             else if (_includeExcludeManager.HasOneItemToInclude())
             {
                 string itemToInclude = _includeExcludeManager.GetFirstItemToInclude().Replace("'", "");
-                command = $"axe.a11yCheck('{itemToInclude}', {Options}, arguments[arguments.length - 1]);";
+                commandParams = Options == "null" ? $"'{itemToInclude}'" : $"'{itemToInclude}', {Options}";
             }
             else
             {
-                command = $"axe.a11yCheck(document, {Options}, arguments[arguments.length - 1]);";
+                commandParams = Options == "null" ? $"document" : $"document, {Options}";
             }
-
-            return Execute(command);
+            var fullCommand = @"window.promiseResultAccessibility = null;
+                                axe.run([params]).then(function (result) {
+                                promiseResultAccessibility = result;
+                                $(document).add('div').attr('id','accessibilityResultReady');
+                            });
+                        ";
+            fullCommand = fullCommand.Replace("[params]", commandParams);
+            return Execute(fullCommand);
         }
     }
 }
